65b898549bc9f8f73a4fcc1051445891ddf604f4
Kirill Ivanov <43354338+Quereal@users.noreply.github.com>
2021-09-15 15:15:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  nav: true,
+  dots: false,
+  margin: 30,
+  responsive:{
+      768: {
+          items:4
+      },
+      0:{
+          items:1,
+      }
+  },
+  navText: ['<i class="carousel__arrow carousel__arrow_prev fa fa-long-arrow-alt-left" aria-hidden="true"></i>', '<i class="carousel__arrow carousel__arrow_next fa fa-long-arrow-alt-right" aria-hidden="true"></i>']
+})
