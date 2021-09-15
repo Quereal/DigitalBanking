8055ae52cab615deced7af7ac77e5f617527d558
Kirill Ivanov <k.i.ivanov@eaca.me>
2021-09-16 01:16:21 +0500

    1

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ $('.owl-carousel').owlCarousel({
   },
   navText: ['<i class="carousel__arrow carousel__arrow_prev fa fa-long-arrow-alt-left" aria-hidden="true"></i>', '<i class="carousel__arrow carousel__arrow_next fa fa-long-arrow-alt-right" aria-hidden="true"></i>']
 })
+
+$(document).ready(function() {
+  $('.header__burger').click(function() {
+      $('.menu').toggleClass('menu_active');
+      $('.header__burger>svg').toggleClass('fa-times').toggleClass('fa-bars');
+  });
+});
